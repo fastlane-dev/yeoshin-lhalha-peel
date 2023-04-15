afd9288edbb0311e1ffc48084cf0c61f1e6a9ee6
Sinkyo <yhko1988@fastlane.kr>
2023-04-15 10:20:38 +0900

    feat : 폰트 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,82 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+const FaktumTest = localFont({
+  variable: "--font-faktumTest",
+  src: [
+    {
+      path: "../../public/fonts/FaktumTest/Faktum_Test_Thin.otf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/FaktumTest/Faktum_Test_Light.otf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/FaktumTest/Faktum_Test_Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/FaktumTest/Faktum_Test_Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
+
+const Pretendard = localFont({
+  variable: "--font-pretendard",
+  src: [
+    {
+      path: "../../public/fonts/Pretendard/Pretendard-Thin.woff",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard/Pretendard-SemiBold.woff",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard/Pretendard-Regular.woff",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard/Pretendard-Medium.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard/Pretendard-Light.woff",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard/Pretendard-ExtraLight.woff",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard/Pretendard-ExtraBold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard/Pretendard-Bold.woff",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard/Pretendard-Black.woff",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
 
 export default function Home() {
   return (
