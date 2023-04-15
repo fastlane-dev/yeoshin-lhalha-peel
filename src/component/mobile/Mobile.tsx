a98ca9a01824d6f1eae14bc8b3b4fcdf1deac498
Sinkyo <yhko1988@fastlane.kr>
2023-04-16 08:32:43 +0900

    feat : 모바일 추가

--- src/component/mobile/Mobile.tsx
+++ src/component/mobile/Mobile.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
 import React from "react";
+import classes from "@/styles/mobile.module.css";
 
 const Mobile = () => {
-  return <div>Mobile</div>;
+  return (
+    <section className="w-full h-full">
+      <div className="p-8">
+        <Image alt="logo" src={"/images/logo.png"} width={67} height={17} />
+        <div className="whitespace-pre-wrap">{"벗기지 않고\n채워주는"}</div>
+      </div>
+    </section>
+  );
 };
 
 export default Mobile;
